01feb5cb6039766e4e7000daa697fb1376eed2cd
TobiasCernek <tobias.cernek.s@gyarab.cz>
2025-03-28 12:17:04 +0100

    hodina

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Food, Meal, WeightedFood
 
-# Register your models here.
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "kcalPer100g", "proteinPer100g", "carbsPer100g", "fatPer100g", "fiberPer100g"]
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+class WeightedFoodAdmin(admin.ModelAdmin):
+    list_display = ["id", "weight", "get_kcal"]
+
+admin.site.register(Food, FoodAdmin)
+admin.site.register(Meal, MealAdmin)
+admin.site.register(WeightedFood, WeightedFoodAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=300)
+    kcalPer100g = models.IntegerField(null=True, blank=True)
+    proteinPer100g = models.IntegerField(null=True, blank=True)
+    carbsPer100g = models.IntegerField(null=True, blank=True)
+    fatPer100g = models.IntegerField(null=True, blank=True)
+    fiberPer100g = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class WeightedFood(models.Model):
+    weight = models.IntegerField(null=True, blank=True)
+    parentFood = models.ForeignKey(Food, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.parentFood} {self.weight}"
+    
+    def get_kcal(self):
+        return self.weight * self.parentFood.kcalPer100g
+    
+class Meal(models.Model):
+    name = models.CharField(max_length=400)
+    foods = models.ManyToManyField(WeightedFood)
+
+    def __str__(self):
+        return f"{self.name}"
